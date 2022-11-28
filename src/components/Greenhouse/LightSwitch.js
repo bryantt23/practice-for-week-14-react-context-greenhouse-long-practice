c0ad31df7884c734575877816704394ac7a17822
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-11-27 23:47:40 -0800

    switch uses context

--- src/components/Greenhouse/LightSwitch.js
+++ src/components/Greenhouse/LightSwitch.js
@@ -1,10 +1,22 @@
 import './LightSwitch.css';
+import { useTheme } from '../../context/ThemeContext';
 
 function LightSwitch() {
+  const { themeName, setThemeName } = useTheme();
   return (
-    <div className="light-switch day">
-      <div className="on">DAY</div>
-      <div className="off">NIGHT</div>
+    <div className='light-switch day'>
+      <div
+        className={themeName === 'day' ? 'on' : 'off'}
+        onClick={() => setThemeName('day')}
+      >
+        DAY
+      </div>
+      <div
+        className={themeName === 'night' ? 'on' : 'off'}
+        onClick={() => setThemeName('night')}
+      >
+        NIGHT
+      </div>
     </div>
   );
 }
