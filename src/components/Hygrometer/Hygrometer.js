d7ba848c57ba337da930dbc77d64d0dea6554e5f
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-12-02 00:37:18 -0800

    change humidity

--- src/components/Hygrometer/Hygrometer.js
+++ src/components/Hygrometer/Hygrometer.js
@@ -1,19 +1,28 @@
-import ReactSlider from "react-slider";
-import "./Hygrometer.css";
+import ReactSlider from 'react-slider';
+import './Hygrometer.css';
+// Humidity has a default value of 40%
+import { createContext, useContext, useState } from 'react';
+
+export const ClimateContext = createContext();
+
+export const useClimate = () => useContext(ClimateContext);
 
 function Hygrometer() {
+  const [humidity, setHumidity] = useState(40);
   return (
     <section>
       <h2>Hygrometer</h2>
-      <div className="actual-humid">Actual Humidity: {"y"}%</div>
+      <div className='actual-humid'>Actual Humidity: {humidity}%</div>
       <ReactSlider
-        value={10}
-        onAfterChange={(val) => {}}
-        className="hygrometer-slider"
-        thumbClassName="hygrometer-thumb"
-        trackClassName="hygrometer-track"
-        ariaLabel={"Hygrometer"}
-        orientation="vertical"
+        value={humidity}
+        onAfterChange={val => {
+          setHumidity(val);
+        }}
+        className='hygrometer-slider'
+        thumbClassName='hygrometer-thumb'
+        trackClassName='hygrometer-track'
+        ariaLabel={'Hygrometer'}
+        orientation='vertical'
         min={0}
         max={100}
         renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
